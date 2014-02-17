76adfae98f4c2b6e03e067ad563b96650b4eea67
Adam Stokes <adam.stokes@ubuntu.com>
2014-02-16 23:26:23 -0500

    clean up example
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- eg/envinfo.pl
+++ eg/envinfo.pl
@@ -6,19 +6,15 @@ use v5.18.0;
 use Juju::Environment;
 use Mojo::JSON qw(j);
 use Data::Dumper;
-use DDP;
 
 $Data::Dumper::Indent = 1;
 
 my $client = Juju::Environment->new(
     endpoint => 'wss://10.0.3.1:17070/',
-    password => '211fdd69b8942c10cef6cfb8a4748fa4'
+    password => 'bac2d0de80a99bb499c442326a617788'
 );
 $client->login;
 my $_info = $client->info;
-my $_info2 = $client->info;
-my $_info3 = $client->info;
-p $_info->{DefaultSeries};
-p $_info2->{DefaultSeries};
-p $_info3->{UUID};
+print Dumper($_info);
+
 $client->close;
